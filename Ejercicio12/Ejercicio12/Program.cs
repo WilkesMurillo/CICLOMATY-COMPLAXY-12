c3e10017f1755877a46bfb74f9a6cee3c0fb78de
WilkesMurillo <wilkesmurillo@gmail.com>
2022-02-21 19:47:35 -0500

    Taller12

---
+++ Ejercicio12/Ejercicio12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nom= "Wilkes";
+            int NHT=45, VHN =54,HET=0, HEE8, Salario;
+            if(NHT >40)
+            {
+                HET = NHT - 40;
+                if (HET > 8)
+                {
+                    HEE8 = HET - 8;
+                    Salario = 40 * VHN + 16 * VHN + HEE8 * 3 * VHN;
+                }
+                else
+                {
+                    Salario = 40 * VHN + HET * 2 * VHN;
+                }
+            }
+            else
+            {
+                Salario = NHT * VHN;
+
+            }
+            Console.WriteLine("El trabajador " + Nom + "  Devengó: $" + Salario); 
+        }
+    }
+}
